6f576f85c98c179f745031d77913c0cd2be2eaad
younusraza <younusraza909@gmail.com>
2022-01-22 07:01:39 +0500

    Basic setup fro delpyment and serve react from node server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const planetRouter = require('./routes/planets/planets.routes')
 const cors = require('cors')
+const path = require('path')
 
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(cors({
     origin: "http://localhost:3000"
 }))
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+
 app.use(planetRouter)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
+
+
 
 module.exports = app
