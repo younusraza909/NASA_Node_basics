13e03a1eb50b2a2810a7675b31e08693a291c76c
younusraza <younusraza909@gmail.com>
2022-01-27 23:14:29 +0500

    Mongoose Connection with MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const app = require('./app')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
 
 const { loadPlanetData } = require('./models/planets.model')
 
@@ -7,7 +10,22 @@ const server = http.createServer(app)
 
 const PORT = 8000;
 
+// Event For Mongoose Connection
+mongoose.connection.once('open', () => {
+    console.log("Connection Created Sucessfully!")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
+
 async function startServer() {
+    await mongoose.connect(process.env.MONGOOSE_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
     await loadPlanetData()
     server.listen(PORT, () => {
         console.log(`Server Listening at Port:${PORT}`);
